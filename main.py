6b55dc63871f8607b891ba3d6695a885d5968f8d
Caster-T <tadeoperezventuro@gmail.com>
2026-03-11 20:08:58 +0000

    B: added logger and constants

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pygame
+from constants import *
+from logger import log_state
+
 def main():
-    print("Hello from asteroid-game!")
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
